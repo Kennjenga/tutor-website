fae39e3b5a0681050218fa662f50b2cfcdd550d9
Kennjenga <108663105+Kennjenga@users.noreply.github.com>
2023-02-23 14:30:17 +0300

    Add files via upload

---
+++ P4.jsx
@@ -0,0 +1,19 @@
+const { number } = require("prop-types");
+
+let name = 1 > 2 ? "Alice" : "Bob";
+console.log(name);
+console.log(true ? "Alice" : "Bob");
+console.log(false ? "Alice" : "Bob");
+console.log(false ? 0:1);
+let num = 1000;
+let x = num%2;
+if (x==0){
+    console.log(even);
+    alert(even);
+};
+"else"
+   { console.log(odd);
+    alert(odd);
+   };
+
+
